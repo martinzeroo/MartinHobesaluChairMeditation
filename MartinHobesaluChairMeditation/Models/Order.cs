8b71e401af616aac7fd0e712b4965550224965a5
Martin <martinhobesalu@gmail.com>
2022-12-22 10:03:56 +0200

    Gave a modle some names

--- MartinHobesaluChairMeditation/Models/Order.cs
+++ MartinHobesaluChairMeditation/Models/Order.cs
@@ -1,15 +1,19 @@
-﻿namespace MartinHobesaluChairMeditation.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MartinHobesaluChairMeditation.Models
 {
     public class Order
     {
         public int Id { get; set; }
 
         public string? Tone { get; set; }
-
+        [Display(Name = "Completed Amount")]
+        [Range(25, 50)]
         public int CompletedAmount { get; set; } = 0;
-
+        [Display(Name = "Order Amount")]
+        [Range (25,50)]
         public int OrderAmount { get; set; }
-
+        [Display(Name = "Time Of Arrival")]
         public DateTime TimeOfArrival { get; set; }
 
         public int Price { get; set; }
