821187ba4d6c569ac1baa38323f054b90183cc4c
Martin <martinhobesalu@gmail.com>
2023-01-10 12:28:28 +0200

    Fixed create error and completed amount error

--- MartinHobesaluChairMeditation/Models/Order.cs
+++ MartinHobesaluChairMeditation/Models/Order.cs
@@ -8,10 +8,10 @@ namespace MartinHobesaluChairMeditation.Models
 
         public string? Tone { get; set; }
         [Display(Name = "Completed Amount")]
-        [Range(25, 50)]
+
         public int CompletedAmount { get; set; } = 0;
         [Display(Name = "Order Amount")]
-        [Range (25,50)]
+        [Range (1,200)]
         public int OrderAmount { get; set; }
         [Display(Name = "Time Of Arrival")]
         public DateTime TimeOfArrival { get; set; }
