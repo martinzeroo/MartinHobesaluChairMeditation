8ddf75c3dd58f2110bffa065376199a20cd96ba6
Martin Hõbesalu <martinhobesalu@gmail.com>
2023-01-23 13:58:59 +0200

    Created user and admin

--- MartinHobesaluChairMeditation/Program.cs
+++ MartinHobesaluChairMeditation/Program.cs
@@ -10,11 +10,53 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+
+
 var app = builder.Build();
+
+var serviceScope = app.Services.CreateScope();
+var RoleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+var UserManager = serviceScope.ServiceProvider.GetService<UserManager<IdentityUser>>();
+
+
+
+string[] roleNames = { "Admin", "Manager", "Member" };
+IdentityResult roleResult;
+
+foreach (var roleName in roleNames)
+{
+    var roleExist = await RoleManager.RoleExistsAsync(roleName);
+    if (!roleExist)
+    {
+        //create the roles and seed them to the database: Question 1
+        roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+    }
+}
+
+//Here you could create a super user who will maintain the web app
+var poweruser = new IdentityUser
+{
+
+    UserName = builder.Configuration["UserName"],
+    Email = builder.Configuration["UserEmail"],
+};
+//Ensure you have these values in your appsettings.json file
+string userPWD = builder.Configuration["UserPassword"];
+var _user = await UserManager.FindByEmailAsync(builder.Configuration["AdminUserEmail"]);
+
+if (_user == null)
+{
+    var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+    if (createPowerUser.Succeeded)
+    {
+        //here we tie the new user to the role
+        await UserManager.AddToRoleAsync(poweruser, "Admin");
+
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
